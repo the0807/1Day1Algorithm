2976e052ae1031b8061a99161fabb6e4b92b9a0b
the0807 <the0807.eom@gmail.com>
2025-03-10 15:13:26 +0900

    백준 2-5 2884번 "알람 시계"

---
+++ Baekjoon/Ch2/2-5.py
@@ -0,0 +1,13 @@
+# 2884번 "알람 시계"
+
+h, m = map(int, input().split())
+
+if m >= 45:
+    print(h, m-45)
+else:
+    m += 15
+    if h == 0:
+        h = 23
+    else:
+        h -= 1
+    print(h, m)
