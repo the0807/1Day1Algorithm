5984727a841332fbfc2aa9b4a04d25629d4e220d
the0807 <the0807.eom@gmail.com>
2025-03-24 18:11:19 +0900

    백준 4-10 1546번 "평균"

---
+++ Baekjoon/Ch4/4-10.py
@@ -0,0 +1,10 @@
+# 1546번 "평균"
+
+num = int(input())
+score = list(map(int, input().split()))
+max_score = max(score)
+
+for i in range(num):
+    score[i] = score[i]/max_score*100
+
+print(sum(score)/num)
