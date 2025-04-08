3e99eb6e6d93f1f37d506e6df72d257bc7a79641
the0807 <the0807.eom@gmail.com>
2025-04-08 17:48:56 +0900

    백준 7-4 # 2563번 "색종이"

---
+++ Baekjoon/Ch7/7-4.py
@@ -0,0 +1,17 @@
+# 2563번 "색종이"
+
+n = int(input())
+array = [[0] * 100 for _ in range(100)]
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    for i in range(a, a + 10):
+        for j in range(b, b + 10):
+            array[i][j] = 1
+            
+cnt = 0
+
+for i in range(100):
+    cnt += array[i].count(1)
+
+print(cnt)
